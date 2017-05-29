8147e4f41c21bd0f8f61dc4a4ae2f5c61a70022c
Vinay Vishal <vinay.vishal@oracle.com>
2017-05-29 12:12:30 +0530

    Commit files for sample ear application
    A sample enterprise application comprising and ejb and servlet. The idea basically is to
    validate the context path of the application and the web module within it in various scenarios.

---
+++ helloworld/ejb-impl/src/main/java/com/example/helloworld/ejb/impl/HelloImpl.java
@@ -0,0 +1,12 @@
+package com.example.helloworld.ejb.impl;
+
+import com.example.helloworld.ejb.api.Hello;
+
+
+public class HelloImpl implements Hello {
+    
+    @Override
+    public String hello() {
+        return "Hello World!";
+    }
+}
