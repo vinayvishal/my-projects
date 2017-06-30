9062e5596526c60593b6921887933c97ff370d9a
Vinay Vishal <vinay.vishal@oracle.com>
2017-06-30 09:01:42 +0530

    Add sample app demonstrating database connectivity using jndi
    Uses derbydb, populates database and then produces result.

---
+++ javaee-samples/wars/datasource-jndi/src/main/java/com/example/DatabaseSetup.java
@@ -0,0 +1,51 @@
+package com.example;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import javax.annotation.Resource;
+import javax.annotation.sql.DataSourceDefinition;
+import javax.ejb.Singleton;
+import javax.ejb.Startup;
+import javax.sql.DataSource;
+
+@Singleton
+@Startup
+public class DatabaseSetup {
+    
+    @Resource(lookup="java:comp/DefaultDataSource")	
+    private DataSource dataSource;
+
+    @PostConstruct
+    public void init() {
+        
+        executeUpdate(dataSource, "CREATE TABLE employee (name VARCHAR(64) PRIMARY KEY, org VARCHAR(64))");
+        
+        executeUpdate(dataSource, "INSERT INTO employee VALUES('mohan', 'oracle')");
+        executeUpdate(dataSource, "INSERT INTO employee VALUES('rohan', 'oracle')");
+        executeUpdate(dataSource, "INSERT INTO employee VALUES('sohan', 'oracle')");
+    }
+    
+    @PreDestroy
+    public void destroy() {
+    	try {
+    		executeUpdate(dataSource, "DROP TABLE employee");
+    	} catch (Exception e) {
+    		// silently ignore, concerns in-memory database
+    	}
+    }
+    
+    private void executeUpdate(DataSource dataSource, String query) {
+        try (Connection connection = dataSource.getConnection()) {
+            try (PreparedStatement statement = connection.prepareStatement(query)) {
+                statement.executeUpdate();
+            }
+        } catch (SQLException e) {
+           throw new IllegalStateException(e);
+        }
+    }
+    
+}
