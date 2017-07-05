a6f2126a9ab432451dd595c3b112d9852952b2d1
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-06 01:53:00 +0530

    Introduce a different scenario
    In this scenario, name has been specified in @Stateless annotation.
    However, same name has also been specified in ejb-jar.xml and
    sun-ejb-jar.xml
    
    This scenario worked.
    ---
    vinay-20170706-015110$ ./asadmin deploy /home/vinay/work/github/samples/javaee-samples/ears/ejb-name-validation/ear/target/helloworld.ear
    Application deployed with name helloworld.
    Command deploy executed successfully.
    vinay-20170706-015114$ curl -v -X GET http://localhost:8080/webapp1
    * Hostname was NOT found in DNS cache
    *   Trying 127.0.0.1...
    * Connected to localhost (127.0.0.1) port 8080 (#0)
    > GET /webapp1 HTTP/1.1
    > User-Agent: curl/7.35.0
    > Host: localhost:8080
    > Accept: */*
    >
    < HTTP/1.1 200 OK
    * Server GlassFish Server Open Source Edition  5.0 is not blacklisted
    < Server: GlassFish Server Open Source Edition  5.0
    < X-Powered-By: Servlet/3.1 JSP/2.3 (GlassFish Server Open Source Edition  5.0  Java/Oracle Corporation/1.8)
    < Content-Length: 31
    <
    AnnotatedHello -->Hello World!
    * Connection #0 to host localhost left intact
    vinay-20170706-015143$
    
    ---

--- javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
+++ javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
@@ -16,15 +16,15 @@ public class HelloServlet extends HttpServlet {
 //    @EJB
 //    private Hello defaultHello;
 
-//    @EJB(beanName = "helloEjbAnnotationName")
-//    private Hello annotatedHello;
+    @EJB(beanName = "helloEjbAnnotationName")
+    private Hello annotatedHello;
 
-    @EJB(beanName = "HelloImpl")
-    private Hello descriptorHello1;
+//    @EJB(beanName = "HelloImpl")
+//    private Hello descriptorHello1;
 
 
-    @EJB(beanName = "HelloImpl2")
-    private Hello descriptorHello2;
+//    @EJB(beanName = "HelloImpl2")
+//    private Hello descriptorHello2;
 
 //    @EJB(lookup = "ejb/com_example_ejb_name_validation_hello")
 //    private Hello subEjbDescriptorHello1;
@@ -33,9 +33,9 @@ public class HelloServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
-//        resp.getWriter().write("AnnotatedHello -->" + annotatedHello.hello() + "\n");
-        resp.getWriter().write("DescriptorHello1 -->" + descriptorHello1.hello() + "\n");
-        resp.getWriter().write("DescriptorHello2 -->" + descriptorHello2.hello() + "\n");
+        resp.getWriter().write("AnnotatedHello -->" + annotatedHello.hello() + "\n");
+//        resp.getWriter().write("DescriptorHello1 -->" + descriptorHello1.hello() + "\n");
+//        resp.getWriter().write("DescriptorHello2 -->" + descriptorHello2.hello() + "\n");
 //        resp.getWriter().write("sunEjbDescriptorHello1 -->" + subEjbDescriptorHello1.hello() + "\n");
 
     }
