a28c9a87e65d1d7f72a8ac895527091946e4e30c
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-06 01:04:42 +0530

    Try to inject EJB using jndi-name in sun-ejb-jar
    Lookup fails in this case.
    
    Exception
    ---
      StandardWrapperValve[com.example.helloworld.web.HelloServlet]: Allocate exception for servlet com.example.helloworld.web.HelloServlet
    javax.naming.NameNotFoundException: com_example_ejb_name_validation_hello not found
            at com.sun.enterprise.naming.impl.TransientContext.doLookup(TransientContext.java:237)
            at com.sun.enterprise.naming.impl.TransientContext.lookup(TransientContext.java:204)
            at com.sun.enterprise.naming.impl.SerialContextProviderImpl.lookup(SerialContextProviderImpl.java:66)
            at com.sun.enterprise.naming.impl.LocalSerialContextProviderImpl.lookup(LocalSerialContextProviderImpl.java:114)
            at com.sun.enterprise.naming.impl.SerialContext.lookup(SerialContext.java:478)
            at com.sun.enterprise.naming.impl.SerialContext.lookup(SerialContext.java:438)
            at javax.naming.InitialContext.lookup(InitialContext.java:417)
            at javax.naming.InitialContext.lookup(InitialContext.java:417)
            at com.sun.ejb.EjbNamingReferenceManagerImpl.resolveEjbReference(EjbNamingReferenceManagerImpl.java:108)
            at com.sun.enterprise.container.common.impl.ComponentEnvManagerImpl$EjbReferenceProxy.create(ComponentEnvManagerImpl.java:1015)
            at com.sun.enterprise.naming.impl.GlassfishNamingManagerImpl.lookup(GlassfishNamingManagerImpl.java:745)
            at com.sun.enterprise.naming.impl.GlassfishNamingManagerImpl.lookup(GlassfishNamingManagerImpl.java:715)
    ---

--- javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
+++ javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
@@ -23,8 +23,8 @@ public class HelloServlet extends HttpServlet {
 //    @EJB(beanName = "HelloImpl2")
 //    private Hello descriptorHello2;
 
-//    @EJB(lookup = "com_example_ejb_name_validation_hello")
-//    private Hello subEjbDescriptorHello1;
+    @EJB(lookup = "com_example_ejb_name_validation_hello")
+    private Hello subEjbDescriptorHello1;
 
 
     @Override
@@ -32,7 +32,7 @@ public class HelloServlet extends HttpServlet {
         resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
         resp.getWriter().write("DescriptorHello1 -->" + descriptorHello1.hello() + "\n");
 //        resp.getWriter().write("DescriptorHello2 -->" + descriptorHello2.hello() + "\n");
-//        resp.getWriter().write("sunEjbDescriptorHello1 -->" + subEjbDescriptorHello1.hello() + "\n");
+        resp.getWriter().write("sunEjbDescriptorHello1 -->" + subEjbDescriptorHello1.hello() + "\n");
 
     }
 }
