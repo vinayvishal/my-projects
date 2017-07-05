e7df6796d60dbbadee70257fb861a2886af77078
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-06 01:59:30 +0530

    Introduce one more scenario
    This fails.
    
    ---
    vinay-20170706-015711$ ./asadmin deploy /home/vinay/work/github/samples/javaee-samples/ears/ejb-name-validation/ear/target/helloworld.ear
    remote failure: Error occurred during deployment: Exception while deploying the app [helloworld] : Cannot resolve reference [Remote ejb-ref name=com.example.helloworld.web2.HelloServlet2/hello,Remote 3.x interface =com.example.helloworld.ejb.api.Hello,ejb-link=null,lookup=,mappedName=,jndi-name=,refType=Session] because there are [2] ejbs in the application with interface com.example.helloworld.ejb.api.Hello.
    Some of the possible causes:
    1. The EJB bean class was packaged in an ear lib library (or through any other library mechanism which makes the library visible to all component modules), this makes all the component modules include this bean class indirectly.
    2. The EJB bean class was packaged in a component module which references the EJB, either directly or indirectly through Manifest, WEB-INF/lib.
    The EJB bean class should only be packaged in the declaring ejb module and not the referencing modules. The referencing modules should only include EJB interfaces.. Please see server.log for more details.
    Command deploy failed.
    
    ---

--- javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
+++ javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
@@ -23,8 +23,8 @@ public class HelloServlet extends HttpServlet {
 //    private Hello descriptorHello1;
 
 
-//    @EJB(beanName = "HelloImpl2")
-//    private Hello descriptorHello2;
+    @EJB(beanName = "HelloImpl2")
+    private Hello descriptorHello2;
 
 //    @EJB(lookup = "ejb/com_example_ejb_name_validation_hello")
 //    private Hello subEjbDescriptorHello1;
@@ -35,7 +35,7 @@ public class HelloServlet extends HttpServlet {
 //        resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
         resp.getWriter().write("AnnotatedHello -->" + annotatedHello.hello() + "\n");
 //        resp.getWriter().write("DescriptorHello1 -->" + descriptorHello1.hello() + "\n");
-//        resp.getWriter().write("DescriptorHello2 -->" + descriptorHello2.hello() + "\n");
+        resp.getWriter().write("DescriptorHello2 -->" + descriptorHello2.hello() + "\n");
 //        resp.getWriter().write("sunEjbDescriptorHello1 -->" + subEjbDescriptorHello1.hello() + "\n");
 
     }
