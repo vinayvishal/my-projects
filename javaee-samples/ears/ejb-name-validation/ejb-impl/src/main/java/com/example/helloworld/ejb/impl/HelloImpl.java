11c747580015c14bb233be45aec92ca647e37247
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-06 23:25:48 +0530

    Inject a jms resource

--- javaee-samples/ears/ejb-name-validation/ejb-impl/src/main/java/com/example/helloworld/ejb/impl/HelloImpl.java
+++ javaee-samples/ears/ejb-name-validation/ejb-impl/src/main/java/com/example/helloworld/ejb/impl/HelloImpl.java
@@ -1,10 +1,16 @@
 package com.example.helloworld.ejb.impl;
 
 import com.example.helloworld.ejb.api.Hello;
+
+import javax.annotation.Resource;
 import javax.ejb.Stateless;
+import javax.jms.Queue;
 
 @Stateless
 public class HelloImpl implements Hello {
+
+    @Resource(name="myQueue")
+    private javax.jms.Queue queue1;
     
     @Override
     public String hello() {
