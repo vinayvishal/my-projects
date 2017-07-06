bac8ace0815f9cbceae3bfdc4d228f384c692a25
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-07 02:16:10 +0530

    Specify a name for jms queue

--- javaee-samples/ears/ejb-name-validation/ejb-impl/src/main/java/com/example/helloworld/ejb/impl/HelloImpl.java
+++ javaee-samples/ears/ejb-name-validation/ejb-impl/src/main/java/com/example/helloworld/ejb/impl/HelloImpl.java
@@ -9,11 +9,12 @@ import javax.jms.Queue;
 @Stateless
 public class HelloImpl implements Hello {
 
-    // A jms queue resource should be pre-configured and bound to jndi name:
-    // java:comp/env/com.example.helloworld.ejb.impl.HelloImpl/queue1
-
-    @Resource(name = "myQueue")
-    private javax.jms.Queue queue1;
+    /*
+     * A jms queue resource should be pre-configured and bound to jndi name:
+     * java:comp/env/myQueue
+     */
+    @Resource(name="myQueue")
+    private javax.jms.Queue myQueue;
     
     @Override
     public String hello() {
--- javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
+++ javaee-samples/ears/ejb-name-validation/war/src/main/java/com/example/helloworld/web/HelloServlet.java
@@ -1,25 +1,25 @@
-package com.example.helloworld.web;
+    package com.example.helloworld.web;
 
-import com.example.helloworld.ejb.api.Hello;
+    import com.example.helloworld.ejb.api.Hello;
 
-import javax.ejb.EJB;
-import javax.servlet.ServletException;
-import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
+    import javax.ejb.EJB;
+    import javax.servlet.ServletException;
+    import javax.servlet.annotation.WebServlet;
+    import javax.servlet.http.HttpServlet;
+    import javax.servlet.http.HttpServletRequest;
+    import javax.servlet.http.HttpServletResponse;
+    import java.io.IOException;
 
-@WebServlet(urlPatterns = "/")
-public class HelloServlet extends HttpServlet {
+    @WebServlet(urlPatterns = "/")
+    public class HelloServlet extends HttpServlet {
 
-    @EJB(beanName = "HelloImpl")
-    private Hello defaultHello;
+        @EJB(beanName = "HelloImpl")
+        private Hello defaultHello;
 
 
-    @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
+        @Override
+        protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+            resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
+        }
     }
-}
 
