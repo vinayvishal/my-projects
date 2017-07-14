e680cd0ffa29f5836e1a6955ef51f602d5278f4f
Vinay Vishal <vinay.vishal@oracle.com>
2017-07-14 14:18:06 +0530

    Add README.md

--- javaee-samples/ears/app-client-ear/README.md
+++ javaee-samples/ears/app-client-ear/README.md
@@ -1,1 +1,17 @@
-## Sample app to reproduce and validate the behavior as explained in glassfish #21466
+# Application with client
+
+```bash
+mvn clean package
+```
+```bash
+$ asadmin deploy --retrieve . ear/target/helloworld.ear 
+Application deployed with name helloworld.
+Command deploy executed successfully.
+```
+Client jar helloworldClient.jar gets generated in the directory specified.
+
+```bash
+$ appclient -jar helloworldClient.jar
+My Bean going to say hello
+Appclient saying Hello World!
+```
--- javaee-samples/ears/app-client-ear/war/src/main/java/com/example/helloworld/web/HelloServlet.java
+++ javaee-samples/ears/app-client-ear/war/src/main/java/com/example/helloworld/web/HelloServlet.java
@@ -13,13 +13,13 @@
     @WebServlet(urlPatterns = "/")
     public class HelloServlet extends HttpServlet {
 
-        @EJB(beanName = "HelloImpl")
-        private Hello defaultHello;
+        @EJB(beanName = "myBean")
+        private Hello myHelloBean;
 
 
         @Override
         protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-            resp.getWriter().write("DefaultHello -->" + defaultHello.hello() + "\n");
+            resp.getWriter().write("DefaultHello -->" + myHelloBean.hello() + "\n");
         }
     }
 
